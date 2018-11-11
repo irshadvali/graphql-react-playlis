829dc26d37b432a9bcd7b247036233daab9f8ef4
irshadvali <irshad.vali@gmail.com>
2018-11-11 16:21:34 +0530

    ui

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
       <div id="main">
-      <h1>irshad's Reading book</h1>
+      <h1>Irshad's Reading book</h1>
       <BookList/>
       <AddBook/>
       </div>
